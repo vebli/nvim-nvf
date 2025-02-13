bc430f58fec28d8e5b599e3bc826de03946765df
vebly <>
2025-02-13 15:06:01 +0100

    added overlay

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     nvf.url = "github:notashelf/nvf";
   };
-  outputs = { self, nixpkgs, nixpkgs-unstable, nvf, ...}: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, nvf, ...}: rec {
+    overlays.default = (super: self: {
+      nvim-custom = packages.${super.system}.default;
+      });
     packages."x86_64-linux".default = (
     nvf.lib.neovimConfiguration{
       pkgs = nixpkgs.legacyPackages."x86_64-linux";
